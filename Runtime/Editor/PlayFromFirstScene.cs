5792e504df163ea8e88f6c22060f6baa7e9a9335
Laura Pincombe <laura@bom.org.uk>
2025-01-08 12:50:48 +0000

    fix: added namespace to file

--- Runtime/Editor/PlayFromFirstScene.cs
+++ Runtime/Editor/PlayFromFirstScene.cs
@@ -1,27 +1,30 @@
-#if UNITY_EDITOR
-using UnityEngine;
-using UnityEditor;
-using UnityEditor.SceneManagement;
-
-public class PlayFromFirstScene : EditorWindow
+namespace Utilities.Editor
 {
-    [MenuItem("Tools/Play From First Scene")]
-    public static void PlayFromFirstSceneMenu()
-    {
-        if (EditorBuildSettings.scenes.Length > 0)
-        {
-            string firstScenePath = EditorBuildSettings.scenes[0].path;
+#if UNITY_EDITOR
+    using UnityEngine;
+    using UnityEditor;
+    using UnityEditor.SceneManagement;
 
-            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+    public class PlayFromFirstScene : EditorWindow
+    {
+        [MenuItem("Tools/Play From First Scene")]
+        public static void PlayFromFirstSceneMenu()
+        {
+            if (EditorBuildSettings.scenes.Length > 0)
             {
-                EditorSceneManager.OpenScene(firstScenePath);                
-                EditorApplication.isPlaying = true;
+                string firstScenePath = EditorBuildSettings.scenes[0].path;
+
+                if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                {
+                    EditorSceneManager.OpenScene(firstScenePath);
+                    EditorApplication.isPlaying = true;
+                }
+            }
+            else
+            {
+                Debug.LogError("No scenes are listed in the Build Settings.");
             }
         }
-        else
-        {
-            Debug.LogError("No scenes are listed in the Build Settings.");
-        }
     }
-}
 #endif
+}
